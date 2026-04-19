7c9eec285b342a016edffc8fab9b0a81a6798038
Fais <ahmadfaisfauzan1123@gmail.com>
2026-04-19 22:45:27 +0800

    chore: update prisma client to support turso authToken

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const adapter = new PrismaLibSql({
+const libsql = createClient({
   url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
 });
+
+const adapter = new PrismaLibSQL(libsql);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
